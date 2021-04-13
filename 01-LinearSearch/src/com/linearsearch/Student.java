1fe828a05928bdc000cf604fd03ffa4d4b64feff
sorozy <sorozy@163.com>
2021-04-13 11:11:43 +0800

    delete

---
+++ 01-LinearSearch/src/com/linearsearch/Student.java
@@ -0,0 +1,46 @@
+public class Student {
+    private Integer age;
+    private String name;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals (Object stu) {
+        if (this == stu) {
+            return true;
+        }
+
+        if (this.getClass() != stu.getClass()) {
+            return false;
+        }
+
+        if (stu == null) {
+            return false;
+        }
+
+        Student another = (Student)stu;
+        return this.name.equals(another.name) && this.age == another.age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
